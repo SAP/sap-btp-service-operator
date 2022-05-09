a15cc0aaab3b9fc410f0f4f679cadf7b73258e60
kerenlahav <45451976+kerenlahav@users.noreply.github.com>
2022-05-09 09:33:29 +0300

    fix nil pointer when last operation is not available (#164)
    * fix nil pointer when last operation is not available

--- controllers/serviceinstance_controller.go
+++ controllers/serviceinstance_controller.go
@@ -382,7 +382,14 @@ func (r *ServiceInstanceReconciler) resyncInstanceStatus(ctx context.Context, sm
 		k8sInstance.Status.Tags = tags
 	}
 
-	switch smInstance.LastOperation.State {
+	instanceState := smTypes.SUCCEEDED
+	if smInstance.LastOperation != nil {
+		instanceState = smInstance.LastOperation.State
+	} else if !smInstance.Ready {
+		instanceState = smTypes.FAILED
+	}
+
+	switch instanceState {
 	case smTypes.PENDING:
 		fallthrough
 	case smTypes.IN_PROGRESS:
