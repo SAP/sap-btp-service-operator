032d9e924a63da366fce2eef39395a4c2143ffd7
i355594 <maya.sigal@sap.com>
2022-11-23 16:38:59 +0200

    fix kubebuilder version

--- hack/install-kubebuilder.sh
+++ hack/install-kubebuilder.sh
@@ -2,7 +2,7 @@ os=$(go env GOOS)
 arch=$(go env GOARCH)
 
 # download kubebuilder and extract it to tmp
-curl -L https://github.com/kubernetes-sigs/kubebuilder/releases/download/v3.7.0/kubebuilder_3.7.0_${os}_${arch}.tar.gz | tar -xz -C /tmp/
+curl -L https://github.com/kubernetes-sigs/kubebuilder/releases/download/v3.7.0/kubebuilder_${os}_${arch}.tar.gz | tar -xz -C /tmp/
 
 # move to a long-term location and put it on your path
 # (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
