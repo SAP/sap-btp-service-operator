48efe4d85285bb47821675efd7ef458da8c65aa9
Naama Solomon <naama.solomon@sap.com>
2022-11-27 12:01:53 +0200

    Revert "update go"
    This reverts commit 8fa1dc96

--- hack/install-kubebuilder.sh
+++ hack/install-kubebuilder.sh
@@ -1,11 +1,2 @@
-os=$(go env GOOS)
-arch=$(go env GOARCH)
-
-# download kubebuilder and extract it to tmp
-#curl -L https://github.com/kubernetes-sigs/kubebuilder/releases/download/v3.7.0/kubebuilder_${os}_${arch}.tar.gz | tar -xz -C /tmp/
-curl -L -o kubebuilder https://github.com/kubernetes-sigs/kubebuilder/releases/download/v3.7.0/kubebuilder_${os}_${arch}
-# move to a long-term location and put it on your path
-# (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
-sudo chmod +x kubebuilder
-sudo mv kubebuilder /usr/local/kubebuilder
-export PATH=$PATH:/usr/local/kubebuilder/bin
+curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)
+chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
