fbde70d5239603b5b6ea65cd6d1e085a6b29655e
Naama Solomon <naama.solomon@sap.com>
2022-11-27 10:24:33 +0200

    update go

--- hack/install-kubebuilder.sh
+++ hack/install-kubebuilder.sh
@@ -8,10 +8,3 @@ curl -L -o kubebuilder https://github.com/kubernetes-sigs/kubebuilder/releases/d
 # (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
 sudo chmod +x kubebuilder
 sudo mv kubebuilder /usr/local/kubebuilder
-
-ETCD_VER=v3.5.6
-curl -L https://github.com/etcd-io/etcd/releases/download/${ETCD_VER}/etcd-${ETCD_VER}-${os}-${arch}.tar.gz -o /tmp/etcd-${ETCD_VER}-${os}-${arch}.tar.gz
-tar xzvf /tmp/etcd-${ETCD_VER}-${os}-${arch}.tar.gz -C /usr/local/bin --strip-components=1
-rm -f /tmp/etcd-${ETCD_VER}-${os}-${arch}.tar.gz
-
-export TEST_ASSET_ETCD=/usr/local/bin
