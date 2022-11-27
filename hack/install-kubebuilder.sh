af5a2719731f04b20a9737218752ed28ad2e3ef0
Naama Solomon <naama.solomon@sap.com>
2022-11-27 12:06:11 +0200

    Revert "update go"
    This reverts commit 8fa1dc96

--- hack/install-kubebuilder.sh
+++ hack/install-kubebuilder.sh
@@ -1,2 +1,3 @@
 curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)
 chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
+export KUBEBUILDER_ASSETS=/usr/local/bin
