145043b075f65a7dd2494d4b6e41e3a2c6baef80
Naama Solomon <naama.solomon@sap.com>
2023-03-19 14:47:19 +0200

    support dc

--- hack/setup-with-kind.sh
+++ hack/setup-with-kind.sh
@@ -2,4 +2,4 @@ make docker-build IMG=localhost:5000/controller:latest
 make docker-push IMG=localhost:5000/controller:latest
 ./hack/kind-with-registry.sh
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.yaml
-make deploy IMG=localhost:5000/controller:latest
+make deploy IMG=localhost:5000/controller
