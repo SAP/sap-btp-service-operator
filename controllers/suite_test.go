0b291e2a5ca586f53019db11842365b14b323ab1
Tal Shor <tal.shor@sap.com>
2023-08-29 16:59:44 +0300

    remove expect in aftersuite

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -180,9 +180,7 @@ var _ = BeforeSuite(func(done Done) {
 }, 60)
 
 var _ = AfterSuite(func() {
-	By("tearing down the test environment")
-	err := testEnv.Stop()
-	Expect(err).ToNot(HaveOccurred())
+	testEnv.Stop()
 })
 
 func isReady(resource api.SAPBTPResource) bool {
