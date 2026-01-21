0801ea5dc7979b4e675024ecf02d751b63fdfe0f
I501080 <keren.lahav@sap.com>
2026-01-21 09:36:54 -0700

    async create operation failure retry

--- controllers/servicebinding_controller.go
+++ controllers/servicebinding_controller.go
@@ -219,6 +219,22 @@ func (r *ServiceBindingReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 		}
 
 		return r.createBinding(ctx, smClient, serviceInstance, serviceBinding)
+	} else { //binding exists in sm but not ready
+		log.Info(fmt.Sprintf("binding %s failed to be created, deleting it", serviceBinding.Status.BindingID))
+		operationURL, unbindErr := smClient.Unbind(serviceBinding.Status.BindingID, nil, utils.BuildUserInfo(ctx, serviceBinding.Spec.UserInfo))
+		if unbindErr != nil {
+			return utils.HandleServiceManagerError(ctx, r.Client, serviceBinding, smClientTypes.DELETE, unbindErr)
+		}
+		if operationURL != "" {
+			log.Info("delete binding request is async")
+			serviceBinding.Status.OperationURL = operationURL
+			serviceBinding.Status.OperationType = smClientTypes.DELETE
+			utils.SetInProgressConditions(ctx, smClientTypes.DELETE, "deleting failed binding", serviceBinding, true)
+			return ctrl.Result{RequeueAfter: r.Config.PollInterval}, utils.UpdateStatus(ctx, r.Client, serviceBinding)
+		}
+		log.Info("binding deleted successfully")
+		serviceBinding.Status.BindingID = ""
+		return ctrl.Result{RequeueAfter: time.Second}, utils.UpdateStatus(ctx, r.Client, serviceBinding)
 	}
 
 	log.Error(fmt.Errorf("update binding is not allowed, this line should not be reached"), "")
