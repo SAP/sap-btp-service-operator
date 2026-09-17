9ba73131e53d2e53decf567beada12ef59d533a4
kerenlahav <45451976+kerenlahav@users.noreply.github.com>
2026-09-17 17:06:20 +0300

    Bug fix - instance/binding is stuck in failed condition after a transient error (#668)
    * binding is stuck in failed condition
    
    * instance is stuck in failed condition
    
    * code review fixes
    
    * .
    
    ---------
    
    Co-authored-by: Naama Solomon <52195888+I065450@users.noreply.github.com>

--- controllers/serviceinstance_controller.go
+++ controllers/serviceinstance_controller.go
@@ -194,6 +194,18 @@ func (r *ServiceInstanceReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 		return r.handleInstanceSharing(ctx, serviceInstance, smClient)
 	}
 
+	if serviceInstance.Status.Ready == "true" {
+		cond := meta.FindStatusCondition(serviceInstance.Status.Conditions, common.ConditionSucceeded)
+		if cond == nil || cond.Status != metav1.ConditionTrue {
+			log.Info("updating instance status 'succeeded' to true")
+			opType := smClientTypes.CREATE
+			if cond != nil && cond.Reason == common.UpdateFailed {
+				opType = smClientTypes.UPDATE
+			}
+			utils.SetSuccessConditions(opType, serviceInstance, false)
+			return ctrl.Result{}, utils.UpdateStatus(ctx, r.Client, serviceInstance)
+		}
+	}
 	log.Info("No action required")
 	return ctrl.Result{}, nil
 }
@@ -701,7 +713,7 @@ func updateRequired(serviceInstance *v1.ServiceInstance) bool {
 	}
 
 	cond := meta.FindStatusCondition(serviceInstance.Status.Conditions, common.ConditionSucceeded)
-	if cond != nil && cond.Reason == common.UpdateInProgress { //in case of transient error occurred
+	if cond != nil && (cond.Reason == common.UpdateInProgress || cond.Reason == common.UpdateFailed) {
 		return true
 	}
 
